35654c28909507285de57caebae16c7e757e9abd
phamlehoang <34034768+phamlehoang@users.noreply.github.com>
2017-12-03 12:58:33 +0700

    Add project files.

---
+++ WebsiteThuongMaiDienTu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteThuongMaiDienTu.Startup))]
+namespace WebsiteThuongMaiDienTu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
